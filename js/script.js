d2d835b04bf93c9239ef23cf83045ca0a1279ee9
Guru prasanna <guruprasanna151@gmail.com>
2025-03-20 14:39:00 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ const eye = document.getElementById("eye");
 function showHide() {
     if(pwd.type=='password'){
         pwd.type="text";
-        eye.src="/icons/eye-open.png"
+        eye.src="icons/eye-open.png"
     }else{
         pwd.type="password";
-         eye.src="/icons/eye-close.png"
+         eye.src="icons/eye-close.png"
     }
 }
 
